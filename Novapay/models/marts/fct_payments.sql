a763c0866593a527201580d3ed84eabe3b4be9d7
hgautam524 <hgautam524@gmail.com>
2026-04-28 17:00:35 +0530

    add fct_payments with net_amount and refund logic; configured incremental merge

---
+++ Novapay/models/marts/fct_payments.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='incremental',
+    schema='DIMENSIONAL',
+    unique_key='transaction_key',
+    incremental_strategy='merge'
+) }}
+
+with enriched_payments as (
+    select * from {{ ref('int_payments_fct') }}
+    
+    {% if is_incremental() %}
+    
+    where _loaded_at >= (select dateadd(day, -3, max(dbt_updated_at)) from {{ this }})
+    {% endif %}
+)
+
+select
+    
+    payment_sk as transaction_key,
+    customer_id,
+    account_id,
+    merchant_id,
+    payment_date as date_key,
+    payment_amount,
+    fee_amount,
+    net_amount,
+    is_refund,
+    
+    _loaded_at as dbt_updated_at
+from enriched_payments
